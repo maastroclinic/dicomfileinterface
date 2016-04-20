5f4cf7b53c6602eaf2753bdb5518a67d2a1245fc
Tim Lustberg <tim.lustberg@maastro.nl>
2016-04-20 14:08:31 +0200

    [DIU-80] added the read_dicomrtstruct stuff to the object, with old names and logic, still need to fix the other bugs

--- classes/RtStruct.m
+++ classes/RtStruct.m
@@ -4,6 +4,11 @@ classdef RtStruct
     %to determine the interpolation grid.
     
     properties (SetAccess  = 'private', GetAccess = 'public')
+        
+        %the variablenames that represent tags do not follow SDT naming conventions 
+        % to stay true to the DICOM naming conventions
+        DicomHeader;
+               
         sliceCollection;
         bitMasks;
         
@@ -27,44 +32,89 @@ classdef RtStruct
         PixelSpacingk;
 
         hasCalcGrid = false;
+        
+        HasForcedStructures;
+        ForcedStructures;
     end
     
     methods
-        function me = RtStruct(struct, pixelSpacing, Origin, Axis, Dimensions)
-            me = me.setPixelSpacing(pixelSpacing);
-            me = me.setOrigin(Origin);
-            me = me.setAxis(Axis);
-            me = me.setDimensions(Dimensions);
-            me = me.parseRTStruct(struct);
+        function this = RtStruct(struct, pixelSpacing, Origin, Axis, Dimensions)
+            if nargin > 0
+                this = this.setPixelSpacing(pixelSpacing);
+                this = this.setOrigin(Origin);
+                this = this.setAxis(Axis);
+                this = this.setDimensions(Dimensions);
+                this = this.parseRTStruct(struct);
+            end
         end
         
-        function out = getRoiMask(me, in)
+        function out = isXio(this)
+            if strcmpi(this.DicomHeader.ManufacturerModelName,'cms, inc.') || ...
+                    strcmpi(this.DicomHeader.ManufacturerModelName,'xio') 
+                out = 1;
+            else
+                out = 0;
+            end
+        end
+        
+        function out = isTrueD(this)
+            if strfind(lower(this.DicomHeader.StructureSetLabel), 'trued') || ...
+                    strfind(lower(this.DicomHeader.SeriesDescription), 'trued') || ...
+                    strfind(lower(this.DicomHeader.StructureSetName), 'trued')
+                out = 1;
+            else
+                out = 0;
+            end
+        end
+        
+        function out = isEsoft(this)
+            if strcmpi(this.DicomHeader.ManufacturerModelName,'Syngo MI Applications') || ...
+                    strcmpi(this.DicomHeader.ManufacturerModelName,'e.soft') 
+                out = 1;
+            else
+                out = 0;
+            end
+        end
+        
+        function out = isMaastroCon(this)
+            if strcmpi(this.DicomHeader.ManufacturerModelName,'MAASTRO clinic') || ...
+                    strcmpi(this.DicomHeader.ManufacturerModelName,'MAASTRO_CON')
+                out = 1;
+            else
+                out = 0;
+            end
+        end
+
+        
+        %% old stuff
+        
+        function out = getRoiMask(this, in)
             
-            [index, name] = me.selectRoiNameIndex(in);
+            [index, name] = this.selectRoiNameIndex(in);
             
-            if me.rendered{index,2} == 0;
+            if this.rendered{index,2} == 0;
                 %suppress the warning on functions genvarname.
                 %it is a deprecate function that can be replace once we
                 %switch to a newer version of Matlab.
-                slices = me.getSlicesForRoi(name);
+                slices = this.getSlicesForRoi(name);
                 if isempty(slices)
                     out = [];
                     return
                 end
-                rawMask = me.calculateBitmask(slices);
+                rawMask = this.calculateBitmask(slices);
                 %bitshift the mask to prevent memory issues
-                me.bitMasks = me.bitMasks + uint64(rawMask.*(2^(index-1)));
-                me.rendered{index,2} = 1;
+                this.bitMasks = this.bitMasks + uint64(rawMask.*(2^(index-1)));
+                this.rendered{index,2} = 1;
             end
-            out = double(bitget(me.bitMasks, index));
+            out = double(bitget(this.bitMasks, index));
         end
 		
-		function image = addRoiToGrayScaleMap(me, image, ctSlice, in)
-            ContourMask = me.getRoiMask(in);
+		function image = addRoiToGrayScaleMap(this, image, ctSlice, in)
+            ContourMask = this.getRoiMask(in);
             ContourMaskSlice = double(flipud(squeeze(ContourMask(:,ctSlice,:))'));
-            ContourList = contourc(double(1:me.Dimensions(1)), double(1:me.Dimensions(3))...
+            ContourList = contourc(double(1:this.Dimensions(1)), double(1:this.Dimensions(3))...
                 , ContourMaskSlice, [1.0 1.0]);
-            ColorMap = me.getColorMap(in);
+            ColorMap = this.getColorMap(in);
             
             limit = size(ContourList,2); i = 1;
             while(i < limit)
@@ -79,10 +129,10 @@ classdef RtStruct
             end
         end
         
-        function sliceNrs = findUsedSlices(me, in, first)
-            ContourMask = me.getRoiMask(in);
+        function sliceNrs = findUsedSlices(this, in, first)
+            ContourMask = this.getRoiMask(in);
             sliceNrs = [];
-            for i = 1:me.Dimensions(2)
+            for i = 1:this.Dimensions(2)
                 ContourMaskSlice = squeeze(ContourMask(:,i,:));
                 if max(max(ContourMaskSlice)) == 1
                     sliceNrs = [sliceNrs, i]; %#ok<AGROW>
@@ -93,104 +143,201 @@ classdef RtStruct
             end
         end
         
-        function out = get.PixelSpacingi(me)
-            out = me.PixelSpacing(1);
+        function out = get.PixelSpacingi(this)
+            out = this.PixelSpacing(1);
         end
         
-        function out = get.PixelSpacingj(me)
-            out = me.PixelSpacing(2);
+        function out = get.PixelSpacingj(this)
+            out = this.PixelSpacing(2);
         end
         
-        function out = get.PixelSpacingk(me)
-            out = me.PixelSpacing(3);
+        function out = get.PixelSpacingk(this)
+            out = this.PixelSpacing(3);
         end
         
-        function out = get.Axis(me)
-            out.Axisi = me.Axisi;
-            out.Axisj = me.Axisj;
-            out.Axisk = me.Axisk;
+        function out = get.Axis(this)
+            out.Axisi = this.Axisi;
+            out.Axisj = this.Axisj;
+            out.Axisk = this.Axisk;
         end 
     end
     
     methods (Access = private)
-%         function me = parseGrid(me, grid)
-%             if grid.hasAllProperties
-%                 me.calcGrid = grid;
-%                 me.bitMasks = zeros(grid.Dimensions ,'uint64');
-%                 me.hasCalcGrid = true;
-%             end
-%         end
-        function me = setPixelSpacing(me, pixelSpacing)
-            me.PixelSpacing = pixelSpacing;
+        function this = setRequiredLabels(this)
+            %the code will crash if these tags are missing from the dicom header
+            %add empty tags if they are missing from the header
+           if ~isfield(this.DicomHeader, 'ManufacturerModelName')
+               this.DicomHeader.ManufacturerModelName = '';
+           end
+           
+           if ~isfield(this.DicomHeader, 'StructureSetLabel')
+               this.DicomHeader.StructureSetLabel = '';
+           end
+           
+           if ~isfield(this.DicomHeader, 'SeriesDescription')
+               this.DicomHeader.SeriesDescription = '';
+           end
+           
+           if ~isfield(this.DicomHeader, 'StructureSetName')
+               this.DicomHeader.StructureSetName = '';
+           end
+           
+           if ~isfield(this.DicomHeader, 'ManufacturerModelName')
+               this.DicomHeader.ManufacturerModelName = '';
+           end
+           
         end
         
-        function me = setOrigin(me, Origin)
-            me.Origin = Origin;
+        function DicomHeader = readDicomFile(~, fileName)
+            if ~exist(fileName, 'file')
+                structString = regexprep(fileName,'\','\\\');
+                throw(MException('Matlab:FileNotFound', ['DICOM RT-STRUCT file ''' structString ''' not found.''']));
+            end
+            %prevent errors with certain structure sets. The
+            %warning is not catch-able so cannot disable only for
+            %the problematic structure sets.
+            DicomHeader = dicominfo(fileName, 'UseVRHeuristic', false);
         end
         
-        function me = setDimensions(me, Dimensions)
-            me.Dimensions = Dimensions;
-            me.bitMasks = zeros(me.Dimensions ,'uint64');
-        end
+        function this = parseDicomHeader(this, DicomHeader)
+            if isfield(DicomHeader, 'StudyInstanceUID')
+                this.DicomHeader = DicomHeader;
+            else
+                throw(MException('RtStruct:InvalidInput',''));
+            end
 
-        function me = setAxis(me, Axis)
-            me.Axisi = Axis.Axisi;
-            me.Axisj = Axis.Axisj;
-            me.Axisk = Axis.Axisk;
-        end
-        
-        function me = parseRTStruct(me, struct)
-            %check if struct is a read RTSTRUCT of filelocation
-            if ~isfield(struct, 'DicomHeader')
-                if ~exist(struct, 'file')
-                    structString = regexprep(struct,'\','\\\');
-                    throw(MException('Matlab:FileNotFound', ['DICOM RT-STRUCT file ''' structString ''' not found.''']));
-                end
-                %prevent errors with certain structure sets. The
-                %warning is not catch-able so cannot disable only for
-                %the problematic structure sets.
-                struct = dicominfo(struct, 'UseVRHeuristic',false);
-                struct = read_dicomrtstruct([] ,'dicomheader', struct);
+            this.HasForcedStructures = 0;
+            this.ForcedStructures = [];
+            for i = 1:length(fieldnames(this.DicomHeader.StructureSetROISequence))
+                disp(i);
+                contours(i) = ...
+                    this.createContourStructure(...
+                        this.DicomHeader.StructureSetROISequence, ...
+                        this.DicomHeader.RTROIObservationsSequence, ...
+                        this.DicomHeader.ROIContourSequence, i); %#ok<AGROW>
             end
             
-            me = me.setSliceCollection(struct.Struct);
-            me = me.parseColorSettingFromHeader(struct.DicomHeader);
-            me = me.setRenderList();
-            me.nrOfRois = length(me.sliceCollection);
+            for i = 1:length(contours)
+                if contours(i).Forced == 1
+                    this.HasForcedStructures = 1;
+                    if isempty(this.ForcedStructures)
+                        this.ForcedStructures = i;
+                    else
+                        this.ForcedStructures(end+1) = i;
+                    end
+                end
+            end
+            
+            this.sliceCollection = contours;
+            this = this.setRequiredLabels();
+            this.nrOfRois = length(this.sliceCollection);
         end
         
-        function me = setRenderList(me)
-           for i = 1:length(me.sliceCollection)
-               me.rendered{i, 1} = me.sliceCollection(i).Name;
-               me.rendered{i, 2} = 0;
+        %TODO, got this function from read_dicomrtstruct, it assumes that item_1 is the same object
+        %in all the sequences.
+        function contour = createContourStructure(~, StructureSetROISequence, RTROIObservationsSequence, ROIContourSequence, i)           
+            iItem   =   ['Item_',num2str(i)];
+            contour.Name    =   StructureSetROISequence.(iItem).ROIName;  
+            contour.Number  =   StructureSetROISequence.(iItem).ROINumber;
+
+            if isfield(StructureSetROISequence.(iItem),'ROIVolume')
+                contour.Volume  =   StructureSetROISequence.(iItem).ROIVolume;
+            else
+                contour.Volume  = 0;
+            end
+            contour.Forced = 0;
+            contour.RelativeElectronDensity = [];
+            if isfield(RTROIObservationsSequence.(iItem),'ROIPhysicalPropertiesSequence')
+               for i = 1  : length(fields(RTROIObservationsSequence.(iItem).ROIPhysicalPropertiesSequence))
+                   contour.Forced = 1;
+                   contour.RelativeElectronDensity = RTROIObservationsSequence.(iItem).ROIPhysicalPropertiesSequence.(['Item_' num2str(i)]).ROIPhysicalPropertyValue;               
+               end
+            end
+
+            if isfield(ROIContourSequence.(iItem), 'ContourSequence')
+                if strcmp(ROIContourSequence.(iItem).ContourSequence.Item_1.ContourGeometricType,'CLOSED_PLANAR')
+                    contour.ClosedPlanar = 1;
+                else
+                    contour.ClosedPlanar = 0;
+                end
+
+                SliceCur        =   1;
+                SliceCurItem    =   'Item_1';
+                while isfield(ROIContourSequence.(iItem).ContourSequence,SliceCurItem)
+                    contour.Slice(SliceCur).X   =   ROIContourSequence.(iItem).ContourSequence.(SliceCurItem).ContourData(1:3:end)/10;
+                    contour.Slice(SliceCur).Y   =   ROIContourSequence.(iItem).ContourSequence.(SliceCurItem).ContourData(3:3:end)/10;
+                    contour.Slice(SliceCur).Z   =   -ROIContourSequence.(iItem).ContourSequence.(SliceCurItem).ContourData(2:3:end)/10;
+                    SliceCur        =   SliceCur+1;
+                    SliceCurItem    =   ['Item_',num2str(SliceCur)];
+                end
+                contour.SliceNum = SliceCur-1;
+            else
+                contour.ClosedPlanar = 0;
+                contour.SliceNum = [];
+                contour.Slice = [];
+            end
+        end        
+        
+        function this = setPixelSpacing(this, pixelSpacing)
+            this.PixelSpacing = pixelSpacing;
+        end
+        
+        function this = setOrigin(this, Origin)
+            this.Origin = Origin;
+        end
+        
+        function this = setDimensions(this, Dimensions)
+            this.Dimensions = Dimensions;
+            this.bitMasks = zeros(this.Dimensions ,'uint64');
+        end
+
+        function this = setAxis(this, Axis)
+            this.Axisi = Axis.Axisi;
+            this.Axisj = Axis.Axisj;
+            this.Axisk = Axis.Axisk;
+        end
+        
+        function this = parseRTStruct(this, struct)
+            %check if struct is a read RTSTRUCT or filelocation
+
+            dcm = this.readDicomFile(struct);
+            this = this.parseDicomHeader(dcm);
+            this = this.parseColorSettingFromHeader(this.DicomHeader);
+            this = this.setRenderList();
+        end
+        
+        function this = setRenderList(this)
+           for i = 1:length(this.sliceCollection)
+               this.rendered{i, 1} = this.sliceCollection(i).Name;
+               this.rendered{i, 2} = 0;
            end
         end
 		
-		function me = setSliceCollection(me, Struct)
+		function this = setSliceCollection(this, Struct)
             for i = 1:length(Struct)
                 index = Struct(i).Number;
                 slices(index) = Struct(i); %#ok<AGROW>
             end
-            me.sliceCollection = slices;
+            this.sliceCollection = slices;
         end
 		
-		function me = parseColorSettingFromHeader(me, header)
-            for i = 1:length(me.sliceCollection)
+		function this = parseColorSettingFromHeader(this, header)
+            for i = 1:length(this.sliceCollection)
                 try
                     index = header.ROIContourSequence.(['Item_' num2str(i)]).ReferencedROINumber;
                     color = header.ROIContourSequence.(['Item_' num2str(i)]).ROIDisplayColor./256;
-                    me.sliceCollection(index).color = color;
+                    this.sliceCollection(index).color = color;
                 catch EM
                     warning(['no colormap found, set to default, MATLAB message: ' EM.message]);
-                    me.sliceCollection(index).color = 'red';
+                    this.sliceCollection(index).color = 'red';
                 end
             end
         end
   
-        function out = calculateBitmask(me, Slices)
+        function out = calculateBitmask(this, Slices)
             %improves readablity of the calculations
-            i = me.PixelSpacingi;
-            k = me.PixelSpacingk;
+            i = this.PixelSpacingi;
+            k = this.PixelSpacingk;
 
             % reshuffle structure data
             Contours = cell(length(Slices),1);
@@ -232,7 +379,7 @@ classdef RtStruct
 
             if length(XStruct) > 1 && length(YStruct) > 1 && length(ZStruct) > 1
                 % re-sample 3D volume
-                out = VolumeResample(structureData,XStruct,YStruct,ZStruct,me.Axisi,me.Axisj,me.Axisk);
+                out = VolumeResample(structureData,XStruct,YStruct,ZStruct,this.Axisi,this.Axisj,this.Axisk);
                 out (out >= 0.5) = 1;
                 out (out < 0.5) = 0;
             else
@@ -241,18 +388,18 @@ classdef RtStruct
             end
         end
         
-        function out = getSlicesForRoi(me, name)
+        function out = getSlicesForRoi(this, name)
             % Extract the slices for the desired structure
-            index = me.getRoiIndexForName(name);
+            index = this.getRoiIndexForName(name);
         
             % Although you would expect only one slice base on the
             % property name, it does contain a set of slices. This is
             % caused by the weird naming scheme in read_dicomrtstruct.m
-            out = me.sliceCollection(index).Slice;
+            out = this.sliceCollection(index).Slice;
         end
         
-        function index = getRoiIndexForName(me, name)
-            index = structfind(me.sliceCollection, 'Name', name);
+        function index = getRoiIndexForName(this, name)
+            index = structfind(this.sliceCollection, 'Name', name);
             if isempty(index)
                 throw(MException('RtStruct:UnknownRoiName', ['The requested ROI name(' name ') was not found in the load RTStruct']));
             end
@@ -265,26 +412,26 @@ classdef RtStruct
             end
         end
                         
-		function out = getColorMap(me, in)
+		function out = getColorMap(this, in)
             % Extract the slices for the desired structure
-            [index, ~] = me.selectRoiNameIndex(in);
-            out = me.sliceCollection(index).color;
+            [index, ~] = this.selectRoiNameIndex(in);
+            out = this.sliceCollection(index).color;
             if isempty(out)
                 out = [1;1;0];%default red
             end
         end
         
-        function [index, name] = selectRoiNameIndex(me, in)
+        function [index, name] = selectRoiNameIndex(this, in)
             if ischar(in)
                 name = in;
-                index = me.getRoiIndexForName(name); 
+                index = this.getRoiIndexForName(name); 
             elseif length(in)== 1 && isnumeric(in)
-                if in <= me.nrOfRois
+                if in <= this.nrOfRois
                     index = in;
-                    name = me.sliceCollection(index).Name;
+                    name = this.sliceCollection(index).Name;
                 else
                     throw(MException('RtStruct:InvalidInput', ...
-                    ['the index (' num2str(in) ') provided is not present for this RtStruct(' num2str(length(me.nrOfRois)) ')']));
+                    ['the index (' num2str(in) ') provided is not present for this RtStruct(' num2str(length(this.nrOfRois)) ')']));
                 end
             else
                 throw(MException('RtStruct:InvalidInput', ...

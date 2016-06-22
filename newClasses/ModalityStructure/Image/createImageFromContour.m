73c228575a732e034c52cd94194efeee3a4160fc
Tim Lustberg <tim.lustberg@maastro.nl>
2016-06-22 14:32:53 +0200

    [DIU-80] added empty slices at beginning and end of the contour image before interpolation to prevent the first or left slice from being deleted by interp3

--- newClasses/ModalityStructure/Image/createImageFromContour.m
+++ newClasses/ModalityStructure/Image/createImageFromContour.m
@@ -7,7 +7,7 @@ function [out] = createImageFromContour( contour, pixelSpacingX, pixelSpacingY, 
     out.pixelSpacingY = pixelSpacingY;
     out.pixelSpacingZ = pixelSpacingZ;
     out.realX = (contour.lowerX+0.5*pixelSpacingX:pixelSpacingX:contour.lowerX+0.5*pixelSpacingX+(columns-1)*pixelSpacingX)';
-    out.realY = contour.uniqueY;
+    out.realY = [contour.uniqueY(1) - pixelSpacingY ; contour.uniqueY; contour.uniqueY(end) + pixelSpacingY]; %needed for adding empty slices
     out.realZ = (contour.lowerZ+0.5*pixelSpacingZ:pixelSpacingZ:contour.lowerZ+0.5*pixelSpacingZ+(rows-1)*pixelSpacingZ)';
     out = VolumeOfInterest(out);
 end

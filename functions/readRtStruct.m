5384f2275fe766313d7cf1a1788065fed501e89f
Tim Lustberg <tim.lustberg@maastro.nl>
2016-07-05 16:53:48 +0200

    [DIU-81] deleted old code

--- functions/readRtStruct.m
+++
@@ -1,7 +0,0 @@
-function newStruct = readRtStruct(fileLocation, useVrHeuristics)
-%READRTSTRUCT create te RtStruct object from a file location
-
-        
-    newStruct = Rtstruct();
-    dicomHeader = dicominfo(fileLocation, 'UseVRHeuristic', useVrHeuristics);
-end

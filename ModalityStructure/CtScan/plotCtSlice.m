cab96da7b9a90e442e37ba7303b96f0e15d4f67d
Tim Lustberg <tim.lustberg@maastro.nl>
2016-08-15 10:02:20 +0200

    cloud atlas plotting stuff

--- ModalityStructure/CtScan/plotCtSlice.m
+++ ModalityStructure/CtScan/plotCtSlice.m
@@ -1,51 +1,14 @@
-% %read rtstruct
-% header = dicominfo('D:\TestData\12345\RTSTRUCT\FO-4073997332899944647.dcm');
-% 
-% %find left lung
-% iStr = fieldnames(header.StructureSetROISequence);
-% num = [];
-% for i=1:length(iStr)
-%     if strcmp('Lung_L', header.StructureSetROISequence.(iStr{i}).ROIName)
-%         num = header.StructureSetROISequence.(iStr{i}).ROINumber;
-%         break;
-%     end
-% end
-% if isempty(num)
-%     disp('ROI Lung_L not found')
-%     return;
-% end
-% 
-% %find data for left lung
-% iStr = fieldnames(header.ROIContourSequence);
-% data = [];
-% for i=1:length(iStr)
-%     if header.ROIContourSequence.(iStr{i}).ReferencedROINumber == num
-%         data = header.ROIContourSequence.(iStr{i}).ContourSequence;
-%         break;
-%     end
-% end
-% if isempty(data)
-%     disp('contour data sequence for Lung_L not found')
-%     return;
-% end
-% 
-% %get data for first slice
-% x   =   data.Item_1.ContourData(1:3:end)/10;
-% y   =   data.Item_1.ContourData(3:3:end)/10;
-% z   =   -data.Item_1.ContourData(2:3:end)/10;
+function hFig = plotCtSlice(ctSlice, hFig)
+    if nargin == 1
+        hFig = figure;
+    end
+    
+    setColorMap();
+    imagesc(ctSlice.pixelData);
+end
 
-%read dicom data
-data = double(dicomread('D:\TestData\12345\CT\FO-169300684774812893.dcm'));
-header = dicominfo('D:\TestData\12345\CT\FO-169300684774812893.dcm');
-
-%transform image
-data = double((data * header.RescaleSlope) + header.RescaleIntercept);
-% data = data.*3; %upscaled the contrast
-figure;
-imagesc(data);
-scale = gray(2000);
-color = scale(1:end, :);
-colormap(color);
-
-
-
+function setColorMap()
+    scale = gray(2000);
+    color = scale(1:end, :);
+    colormap(color);
+end
---
+++ ModalityStructure/Image/addContourToFigureFromVoi.m
@@ -0,0 +1,29 @@
+%this function is used to add a contour to an UNALTERED figure produced by 
+% by the plotSliceOfCtScan function
+function hFig = addContourToFigureFromVoi(voi, sliceNr, lineSpec, hFig)
+    if nargin == 2
+        lineSpec = '-b';
+        hFig = figure;
+    end
+    
+    if sliceNr > voi.slices
+        MException('MATLAB:dicom-file-interface:addContourToFigureFromVoi', 'Selected slice is not present in VOI');
+    end
+    
+    slice = double(squeeze(voi.uncompressedPixelData(:,sliceNr,:)));
+    drawContour = contourc(1:512,1:512, slice , [.5 .5]);
+    
+    indexOfLastContour = size(drawContour,2);
+    i = 1;
+    
+    hold on;
+    while i < indexOfLastContour
+        pointsInCurrentContour = drawContour(2,i);
+        xContour = drawContour(2, i+1:i+pointsInCurrentContour);
+        zContour = drawContour(1, i+1:i+pointsInCurrentContour);
+        plot(zContour,xContour, lineSpec);
+        i = pointsInCurrentContour+1; %go to next contour
+    end
+    hold off;
+end
+

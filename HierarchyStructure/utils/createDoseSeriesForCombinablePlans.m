a4425cce8475eaddf411a2b06dda5ea98ae9504d
Tim Lustberg <tim.lustberg@maastro.nl>
2017-01-05 10:35:54 +0100

    [CA-5] added a function to create a DICOM series that contains a copy of the summed dose for every plan that it refers to. this is to prevent one RTDOSE being linked to multiple plans. the PixelData is identical for each RTDOSE in the series but the header is slightly different

---
+++ HierarchyStructure/utils/createDoseSeriesForCombinablePlans.m
@@ -0,0 +1,34 @@
+function [series, pixelData] = createDoseSeriesForCombinablePlans(patient, planUids)
+%CREATECOMBINEDDOSE add a combined dicom set to patient
+    series = Series();
+    pixelData = [];
+    
+    nrOfPlans = length(planUids);
+    if nrOfPlans == 1
+        warning('only one plan provided, returned original RtDose for this plan');
+        return;
+    end
+    
+    dose = RtDose();
+    for i = 1:nrOfPlans
+        dose(i) = getRtDoseForPlan(patient, planUids{i});
+    end
+    [pixelData, doseGridScaling] = combineDosePixelData(dose);
+
+    
+    newSeriesUid = dicomuid;
+    newSeriesDescription = 'Combined plan RTDOSE collection';
+    for i = 1:length(dose)
+        newDose = newDoseHeader(dose(i), newSeriesUid, newSeriesDescription, doseGridScaling);
+        series = series.parseDicomObj(newDose);
+    end
+end
+
+function dose = newDoseHeader(dose, newSeriesUid, newSeriesDescription, doseGridScaling)
+    dose.dicomHeader.SOPInstanceUID = dicomuid;
+    dose.dicomHeader.SeriesInstanceUid = newSeriesUid;
+    dose.dicomHeader.SeriesDescription = newSeriesDescription;
+    dose.dicomHeader.ManufacturerModelName = 'PlanDoseCombiner';
+    dose.dicomHeader.Manufacturer = 'MAASTRO SDT';
+    dose.dicomHeader.DoseGridScaling = doseGridScaling;
+end
